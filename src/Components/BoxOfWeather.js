be906b9d53d72e324d597f2b0fe4acd511b399ab
NEX-DoT <nex.dot2@gmail.com>
2023-04-25 12:27:31 +0430

    -v0.1

--- src/Components/BoxOfWeather.js
+++ src/Components/BoxOfWeather.js
@@ -8,7 +8,7 @@ export default class BoxOfWeather extends Component {
          <h1>Weather in {this.props.name}  <img src={"https://openweathermap.org/img/wn/"+this.props.icon+".png"} alt="" /></h1>
          <h3>Temp : {this.props.temp}°</h3>
          <h3>Description : {this.props.description}</h3>
-         <h3>humidity : {this.props.humidity}%</h3>
+         <h3>Humidity : {this.props.humidity}%</h3>
          <h3>Wind Speed : {this.props.speed}km</h3>
    </div> 
       </>
